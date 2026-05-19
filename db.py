dc25cd9c34810ef540cbb7b4018ae810ab8e6b5f
mariana-morillo <mori.cp@gmail.com>
2026-05-19 15:28:51 -0300

    Update db.py

--- db.py
+++ db.py
@@ -94,7 +94,8 @@ def excluir_questao(q_id):
         conn_central.commit()
 
 def obter_assuntos_da_disciplina(disciplina):
-    res = conn_central.execute(text("SELECT DISTINCT assunto FROM questoes WHERE disciplina = :d AND assunto IS NOT NULL AND assunto != '' ORDER BY assunto"), {"d": disciplina}).fetchall()
+    # Adicionamos .session antes do .execute
+    res = conn_central.session.execute(text("SELECT DISTINCT assunto FROM questoes WHERE disciplina = :d AND assunto IS NOT NULL AND assunto != '' ORDER BY assunto"), {"d": disciplina}).fetchall()
     return ["Todos"] + [r[0] for r in res]
 
 def buscar_questoes_filtradas(disciplina, limite=None, assunto="Todos", dificuldade="Todos", tipo="Todos", sortear=False, excluir_ids=None, uso="Todos"):
